5d30392d3e71626f47b1c7508ac9eb83db44fd1b
antoniotex <acm.skt@gmail.com>
2022-02-07 21:20:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to.
+          Edit <code>src/App.js</code> and save to teste mirror.
         </p>
         <a
           className="App-link"
