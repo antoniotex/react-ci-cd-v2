e81a858ad321d05fac95b0b401409b3ffc457f03
antoniotex <acm.skt@gmail.com>
2022-02-07 21:49:48 -0300

    Mirror v5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to teste mirror v4.
+          Edit <code>src/App.js</code> and save to teste mirror v5.
         </p>
         <a
           className="App-link"
